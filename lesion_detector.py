4574b9da19b308f32c86be1040bb453d1bb7b1a9
arjuniyer01 <iyer9@wisc.edu>
2023-02-03 18:23:26 -0600

    branch setup + draft settings for grey background

--- lesion_detector.py
+++ lesion_detector.py
@@ -8,6 +8,22 @@ from PIL import ImageFilter
 # Using a Dataframe to save data to a CSV 
 results_df = pd.DataFrame(columns=['image', 'leaf area', 'lesion area', 'percentage of leaf area'])
 
+settings = {
+    'background_colour': 'black_velvet',
+    'black_velvet': {
+        'input_folder_path': "./input_images/black_velvet/",
+        'output_folder_path': "./results/black_velvet/",
+        'leaf_area': {'min_hue': 50, 'max_hue': 100, 'min_saturation': 0, 'max_saturation': 255, 'min_value': 60, 'max_value': 255},
+        'lesion_area': {'min_hue': 45, 'max_hue': 100, 'min_saturation': 0, 'max_saturation': 255, 'min_value': 140, 'max_value': 255},
+        },
+    'grey_background': {
+        'input_folder_path': "./input_images/grey_background/",
+        'output_folder_path': "./results/grey_background/",
+        'leaf_area': {'min_hue': 20, 'max_hue': 100, 'min_saturation': 20, 'max_saturation': 255, 'min_value': 60, 'max_value': 255},
+        'lesion_area': {'min_hue': 20, 'max_hue': 100, 'min_saturation': 20, 'max_saturation': 255, 'min_value': 120, 'max_value': 255},
+        }
+    }
+
 def get_leaf_area_binary(img):
     '''
     This function takes an image as input and returns a binary image with the leaf area highlighted in white.
@@ -18,10 +34,10 @@ def get_leaf_area_binary(img):
     hsv = np.array(hsv_img)
     
     # Create a mask of green regions
-    min_hues = hsv[:,:,0] > 50
-    max_hues = hsv[:,:,0] < 100
-    saturation = hsv[:,:,1] > 0
-    values = hsv[:,:,2] > 60
+    min_hues = hsv[:,:,0] > settings[settings['background_colour']]['leaf_area']['min_hue']
+    max_hues = hsv[:,:,0] < settings[settings['background_colour']]['leaf_area']['max_hue']
+    saturation = hsv[:,:,1] > settings[settings['background_colour']]['leaf_area']['min_saturation']
+    values = hsv[:,:,2] > settings[settings['background_colour']]['leaf_area']['min_value']
     new_img = Image.fromarray(np.uint8(min_hues*max_hues*saturation*values*255))
 
     # Remove noise
@@ -43,10 +59,10 @@ def get_lesion_area_binary(img):
     hsv = np.array(hsv_img)
     
     # Create a mask of lesions
-    min_hues = hsv[:,:,0] > 45
-    max_hues = hsv[:,:,0] < 100
-    saturation = hsv[:,:,1] > 0
-    values = hsv[:,:,2] > 140
+    min_hues = hsv[:,:,0] > settings[settings['background_colour']]['lesion_area']['min_hue']
+    max_hues = hsv[:,:,0] < settings[settings['background_colour']]['lesion_area']['max_hue']
+    saturation = hsv[:,:,1] > settings[settings['background_colour']]['lesion_area']['min_saturation']
+    values = hsv[:,:,2] > settings[settings['background_colour']]['lesion_area']['min_value']
     new_img = Image.fromarray(np.uint8(min_hues*max_hues*saturation*values*255))
 
     # Remove noise
@@ -54,15 +70,18 @@ def get_lesion_area_binary(img):
 
     # Save lesion size to dataframe
     results_df.loc[results_df['image'] == image_name, 'lesion area'] = results_df.loc[results_df['image'] == image_name, 'leaf area'] - np.sum(min_hues*max_hues*saturation*values)
-    results_df.loc[results_df['image'] == image_name, 'percentage of leaf area'] = results_df.loc[results_df['image'] == image_name, 'lesion area'] / results_df.loc[results_df['image'] == image_name, 'leaf area']
+    results_df.loc[results_df['image'] == image_name, 'percentage of leaf area'] = 100*results_df.loc[results_df['image'] == image_name, 'lesion area'] / results_df.loc[results_df['image'] == image_name, 'leaf area']
 
     return new_img.convert('RGB')
 
 if __name__ == '__main__':
 
+    # get settings
+    settings['background_colour'] = input('What is the background colour? (black_velvet or grey_background) ')
+
     # paths used
-    input_folder_path = "./input_images/"
-    output_folder_path = "./results/"
+    input_folder_path = "./input_images/" + settings['background_colour'] + "/"
+    output_folder_path = "./results/" + settings['background_colour'] + "/"
 
     # process images
     for image_name in os.listdir(input_folder_path):
@@ -72,4 +91,6 @@ if __name__ == '__main__':
         get_lesion_area_binary(Image.open(input_folder_path + image_name)).save(output_folder_path+'lesion_area_binaries/' + f'{image_name[:-4]}_lesion_area_binary.jpeg')
 
     # save calculations to output file
-    results_df.to_csv('./results/output.csv', index=False)
+    results_df.to_csv(output_folder_path+'output.csv', index=False)
+
+    
