8dedc8ac1c83ad2341ab948bd5ee259cc2c53e5b
arjuniyer01 <iyer9@wisc.edu>
2023-04-10 16:31:59 -0500

    clustering setup

---
+++ src/leaflesiondetector/clustering.py
@@ -0,0 +1,66 @@
+# for loading/processing the images
+from tensorflow.keras.utils import load_img
+from keras.applications.vgg16 import preprocess_input
+
+# models
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+
+# clustering and dimension reduction
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+import os
+import numpy as np
+from PIL import Image
+
+from leaflesiondetector.leaf import Leaf, LeafList
+
+# holds the cluster id and the images { id: [images] }
+groups = {}
+
+
+def extract_features(img, model):
+    # load the image as a 224x224 array
+    # img = load_img(file, target_size=(224,224))
+    img = img.resize((224, 224), Image.ANTIALIAS)
+    # convert from 'PIL.Image.Image' to numpy array
+    img = np.array(img)
+    # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
+    reshaped_img = img.reshape(1, 224, 224, 3)
+    # prepare image for model
+    imgx = preprocess_input(reshaped_img)
+    # get the feature vector
+    features = model.predict(imgx, use_multiprocessing=True)
+    return features
+
+
+def cluster(x, n, leaflist: LeafList):
+    # cluster feature vectors
+    kmeans = KMeans(n_clusters=n, random_state=25)
+    kmeans.fit(x)
+    for leaf, cluster in zip(leaflist, kmeans.labels_):
+        leaf.cluster = cluster
+
+
+def run(leaflist: LeafList, n: int):
+    """runs the clustering algorithm"""
+    # load the model
+    print("Loading model...")
+    model = VGG16()
+    # re-structure the model
+    print("Re-structuring model...")
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # extract features from each image
+    print("Extracting features from images...")
+    features = [extract_features(leaf.img.copy(), model) for leaf in leaflist]
+    # flatten into a 2D array
+    features = np.array(features).reshape(len(leaflist), 4096)
+    # reduce the features to 2D
+    print("Reducing features to 2D...")
+    pca = PCA(n_components=len(leaflist))
+    pca.fit(features)
+    x = pca.transform(features)
+    # cluster the features
+    print("Clustering features...")
+    cluster(x, n, leaflist)
---
+++ src/leaflesiondetector/pages/Clustering.py
@@ -0,0 +1,70 @@
+import streamlit as st
+from leaflesiondetector import ui_functions
+from leaflesiondetector.leaf import LeafList
+from leaflesiondetector import clustering
+import time
+from streamlit_lottie import st_lottie_spinner
+from collections import defaultdict
+
+st.set_page_config(
+    page_title="Leaf Lesion Detector", page_icon=":leaves:", layout="wide"
+)
+
+st.title(":leaves: Leaf clustering tool")
+
+if "cluster" not in st.session_state:
+    st.session_state["cluster"] = False
+
+if "cluster_leaves" not in st.session_state:
+    st.session_state["cluster_leaves"] = LeafList()
+
+upload_status = st.empty()
+with st.form("my-form", clear_on_submit=True):
+    uploaded_files = st.file_uploader(
+        "Upload images", type=["jpg", "jpeg", "png"], accept_multiple_files=True
+    )
+    num_clusters = st.number_input(
+        "Number of clusters", min_value=2, max_value=10, value=2
+    )
+    submitted = st.form_submit_button("Process Images")
+    if (len(uploaded_files) >= num_clusters) and submitted:
+        st.session_state["cluster_leaves"] = LeafList()
+        st.session_state["maintain"] = False
+        ui_functions.save_uploaded_files(
+            uploaded_files, st.session_state["cluster_leaves"].leaves
+        )
+        st.session_state["cluster"] = True
+    elif (len(uploaded_files) == 0) and submitted:
+        st.session_state["cluster"] = False
+        upload_status.error("Please upload at least one image.")
+        time.sleep(2)  # For Streamlit UI behaviour
+        upload_status.empty()
+    elif (len(uploaded_files) < num_clusters) and submitted:
+        st.session_state["cluster"] = False
+        upload_status.error(
+            "Number of images should be greater than or equal to number of clusters."
+        )
+        time.sleep(2)  # For Streamlit UI behaviour
+
+if st.session_state["cluster"]:
+    st.markdown(f"## Clustering into {num_clusters} clusters")
+    with st_lottie_spinner(
+        ui_functions.load_lottieurl(
+            "https://assets4.lottiefiles.com/packages/lf20_XIIxSb.json"
+        ),
+        key="download",
+        speed=0.5,
+        loop=True,
+    ):
+        clusters = defaultdict(list)
+        print("\n\nclustering\n\n")
+        clustering.run(st.session_state["cluster_leaves"].leaves, num_clusters)
+        print("\n\nclustered\n\n")
+        for leaf in st.session_state["cluster_leaves"].leaves:
+            clusters[leaf.cluster].append(leaf)
+        for cluster in clusters:
+            st.markdown(f"## Cluster {cluster}")
+            cols = st.columns(4)
+            for i, leaf in enumerate(clusters[cluster]):
+                cols[i % 4].markdown(f"#### {leaf.name}")
+                cols[i % 4].image(leaf.img)
